4390a4a8d73c24a138eadee548dae4376ae89a2f
chienBv92 <mcnb.02@gmail.com>
2019-10-12 05:33:49 +0700

    update code user info

--- Web Gio Cha/Models/Define/UserModel.cs
+++ Web Gio Cha/Models/Define/UserModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using Web_Gio_Cha.EF;
+using System.Web.Mvc;
 
 
 namespace Web_Gio_Cha.Models
@@ -11,5 +12,14 @@ namespace Web_Gio_Cha.Models
     public class UserModel: TblUser
     {
         public int USER_ID { get; set; }
+
+        public string DistrictName { get; set; }
+
+        public List<SelectListItem> DISTRICT_LIST { get; set; }
+
+        public string NEW_PASSWORD { get; set; }
+        public string NEW_PASSWORD_REPEAT { get; set; }
+
+
     }
 }
